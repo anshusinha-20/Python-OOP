38a242fca210b9afa7affdf039206db1742b3a08
Anshu Sinha <anshujuly2@gmail.com>
2023-03-04 22:02:55 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -48,20 +48,21 @@
 #####
 
 ## Adding attributes
-"""creating a class named 'Point' """
-class Point:
-    pass
+# """creating a class named 'Point' """
+# class Point:
+#     pass
 
+# """creating two objects"""
+# p1 = Point()
+# p2 = Point()
 
-p1 = Point()
-p2 = Point()
+# """adding attributes to the objects"""
+# p1.x = 5
+# p1.y = 4
 
+# p2.x = 3
+# p2.y = 6
 
-p1.x = 5
-p1.y = 4
+# print(p1.x, p1.y)
+# print(p2.x, p2.y)
 
-p2.x = 3
-p2.y = 6
-
-print(p1.x, p1.y)
-print(p2.x, p2.y)
